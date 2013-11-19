c138bbb67407001a09a4114442787b40b1633358
Andrew Olson <aolson@geezeo.com>
2013-11-18 22:51:39 -0500

    Initial capistrano-ci semaphore client.

--- lib/capistrano/ci.rb
+++ lib/capistrano/ci.rb
@@ -3,6 +3,7 @@ require "capistrano/ci/clients/base"
 require "capistrano/ci/clients/travis"
 require "capistrano/ci/clients/travis_pro"
 require "capistrano/ci/clients/circle"
+require "capistrano/ci/clients/semaphore"
 require "capistrano/ci/client"
 
 Capistrano::CI::Client.register "travis", Capistrano::CI::Clients::Travis, [:ci_repository]
